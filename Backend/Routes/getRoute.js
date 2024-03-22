1be520008a2b5585a93c2e08819c6e4f750152cc
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-03-22 10:03:10 +0700

    tahap 1 pembuatan halaman sign in

--- Backend/Routes/getRoute.js
+++ Backend/Routes/getRoute.js
@@ -21,3 +21,4 @@ router.get('/dashboard', (req, res) => {
 
 export default router;
 
+
--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,11 @@
-import express from 'express';
+import express, { json } from 'express';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import getRoute from "./Routes/getRoute.js";
 
+
 const app = express();
+app.use(express(json));
 const PORT = 3000;
 
 const __filename = fileURLToPath(import.meta.url);
