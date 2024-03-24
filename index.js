bd32d49a6739d4d7617dea92bd3508853ce3ecfa
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-03-24 21:56:43 +0700

    membenarkan routes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import  authenticateToken  from './src/middlewares/authMiddleware.js';
-// import router from './src/routes/authRoute.js';
+import authRouter from './src/routes/authRoute.js';
 import getRouter from './src/routes/getRoute.js';
 
 const app = express();
@@ -10,7 +10,9 @@ app.use(express.json());
 
 // Routes
 app.use('/', getRouter);
-// app.use('/dashboard', router);
+app.use('/dashboard', getRouter);
+app.use('/login',getRouter);
+app.use('/register',getRouter);
 
 const port = 3000;
 app.listen(port, () => {
