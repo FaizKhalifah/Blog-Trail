abbfae873035f22f08a9a607594d52f57ada419a
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-03-25 08:08:21 +0700

    fungsi add dan delete user pada crud

---
+++ src/crud/createUser.js
@@ -0,0 +1,11 @@
+import user from "../models/user.js";   
+ 
+async function  addUser(username,password,interest){
+    const newUser = new user({
+        userName:username,
+        password:password,
+        interest:interest,
+        blogs:[]
+    })
+    await newUser.save();
+}
