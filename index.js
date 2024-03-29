1a1fb0f2c42970b6c1f69b05588047a396a046e5
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-03-29 22:43:20 +0700

    menambah halaman index di html

--- index.js
+++ index.js
@@ -2,17 +2,24 @@ import express from "express";
 import mongoose  from "mongoose";
 import router from "./routes/authRoute.js";
 import cookieParser from "cookie-parser";
+import process from "process";
+
+const currentDirectory = process.cwd();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 const connection ='mongodb://localhost:27017/BlogTrail';
 mongoose.connect(connection)
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
-  app.get('/', (req, res) => res.render('index'));
+ app.get('/',async(req,res)=>{
+  res.sendFile('./public/index.html',{root:currentDirectory});
+ })
+  
   app.get('/set-cookies', (req, res) => {
     res.cookie('newUser', false);
     res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
