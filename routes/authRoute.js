c4bc9d860ecc4d13a799904ab4b896f884e86d00
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-03-31 14:33:03 +0700

    fungsi sign in

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,8 +2,8 @@ import { Router } from "express";
 import authController from "../controllers/authController.js";
 
 const router = Router();
-router.get('/register',authController.signup_get);
-router.post('/register',authController.signup_post);
+router.get('/register',authController.register_get);
+router.post('/register',authController.register_post);
 router.get('/login',authController.login_get);
 router.post('/login',authController.login_post);
 
