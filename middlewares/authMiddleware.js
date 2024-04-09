057ca58832252febf6bf00cbfed836b27fa4c050
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-04-09 14:10:38 +0700

    fungsi check user pada middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
 
 const requireAuth=(req,res,next)=>{
@@ -20,4 +21,23 @@ const requireAuth=(req,res,next)=>{
 
 }
 
-export default requireAuth;
+const checkUser = (req,res,next)=>{
+    const token = req.cookies.jwt;
+    if(token){
+        jwt.verify(token,'tokenRahasia', async(err,decodedToken)=>{
+            if(err){
+                res.locals.user=null;
+                next();
+            }else{
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        })
+    }else {
+        res.locals.user = null;
+        next();
+      }
+}
+
+export default {requireAuth,checkUser};
