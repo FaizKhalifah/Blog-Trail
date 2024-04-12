629c7b30d92fa034bdeb098266e91a98b804f682
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-04-12 23:24:03 +0700

    percobaan 2 fitur update profile

--- views/dashboard/script.js
+++ views/dashboard/script.js
@@ -1,5 +1,7 @@
 const form = document.querySelector("form");
 
+
+
 form.addEventListener('submit',async(e)=>{
     e.preventDefault();
     const email = form.email.value;
@@ -14,9 +16,18 @@ form.addEventListener('submit',async(e)=>{
                 email,username,password,interest
             }),
             headers: {'Content-Type': 'application/json'}
+        }).then(response=>{
+            return response.json();
+        }).then(data=>{
+            document.getElementById('userHeader').textContent = `${user.username}`;
+            document.getElementById('userEmail').textContent = `Email : ${user.email}`;
+            document.getElementById('userInterest').textContent = `Interest : ${user.interest}`;
+            document.getElementById('userBlogs').textContent = `Blogs : ${user.blogs}`;
         });
         console.log(res);
     }catch(err){
         console.log(err);
     }
+
+   
 })
