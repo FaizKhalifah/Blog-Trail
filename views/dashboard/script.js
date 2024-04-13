ce3784e999ad1bb05acd9d0ad53dd8a601d1cc9d
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-04-13 09:22:33 +0700

    membuat fungsi clearform dan editupper pada script dashboard

--- views/dashboard/script.js
+++ views/dashboard/script.js
@@ -1,7 +1,5 @@
 const form = document.querySelector("form");
 
-
-
 form.addEventListener('submit',async(e)=>{
     e.preventDefault();
     const email = form.email.value;
@@ -17,17 +15,25 @@ form.addEventListener('submit',async(e)=>{
             }),
             headers: {'Content-Type': 'application/json'}
         })
-
         const data = await res.json();
         const updatedData = data.updatedData;
-        document.getElementById('userHeader').textContent = `${updatedData.username}`;
-        document.getElementById('userEmail').textContent = `Email : ${updatedData.email}`;
-        document.getElementById('userInterest').textContent = `Interest : ${updatedData.interest}`;
-
+        await editUpper(updatedData);
+        await clearForm(form);
         
     }catch(err){
         console.log(err);
     }
-
    
 })
+
+async function editUpper(updatedData){
+    document.getElementById('userHeader').textContent = `${updatedData.username}`;
+    document.getElementById('userEmail').textContent = `Email : ${updatedData.email}`;
+    document.getElementById('userInterest').textContent = `Interest : ${updatedData.interest}`;
+}
+
+async function clearForm(form){
+    form.email.value='';
+    form.username.value='';
+    form.password.value='';
+}
