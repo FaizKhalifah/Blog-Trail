0869a9431c6ed9a120c01816abc4160df25dd60c
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-05-01 20:58:40 +0700

    fungsi edit blog di crud user dan blog

--- test/crudTest.js
+++ test/crudTest.js
@@ -2,7 +2,6 @@ import userCrud from "../Utils/Crud/userCrud.js";
 import blogCrud from "../Utils/Crud/blogCrud.js";
 
 
-
-await userCrud.deleteBlog("AkuFaiz","111","Blog1","technology");
+await userCrud.editBlog("AkuFaiz","111","Blog 1","Halo semua, ini blog 1 tentang pendidikan");
 
 // await userCrud.addUser("faizganteng@yahoo.com","faizzz","111","sports");
